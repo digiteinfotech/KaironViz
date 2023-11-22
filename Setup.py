78aeb1684af0c293b39a96708f3d3c0b8277a6fc
spandan.mondal <spandan.mondal@nimblework.com>
2023-11-22 15:00:46 +0530

    organized setup.py

--- Setup.py
+++ Setup.py
@@ -4,48 +4,33 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
  
 setuptools.setup(
-    # Here is the module name.
     name="kaironviz",
  
-    # version of the module
     version="0.0.2",
  
-    # Name of Author
     author="Kairon Team, Nimblework",
  
-    # your Email address
     author_email="spandan.mondal@nimblework.com",
  
-    # #Small Description about module
     description="Data visualization for notebooks and html export",
  
-    # long_description=long_description,
- 
-    # Specifying that we are using markdown file for description
     long_description=long_description,
     long_description_content_type="text/markdown",
  
-    # Any link to reach this module, ***if*** you have any webpage or github profile
-    # url="https://github.com/username/",
+
     packages=setuptools.find_packages(),
     include_package_data=True,
- 
- 
-    # if module has dependencies i.e. if your package rely on other package at pypi.org
-    # then you must add there, in order to download every requirement of package
- 
- 
+
  
     install_requires=[
         "pandas",
         "ipython",
         "jsmin",
     ],
- 
+
  
     license="MIT",
  
-    # classifiers like program is suitable for python3, just leave as it is.
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
